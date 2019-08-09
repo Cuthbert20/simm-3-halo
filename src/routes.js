67558d132c62b4f9a6e3b9d5abdf24c32936e054
Cuthbert20 <knowltonspencer@gmail.com>
2019-08-09 10:20:21 -0600

    routes setup

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Auth from '../src/Components/Auth/Auth'
+import Dashboard from '../src/Components/Dashboard/Dashboard'
+import Form from '../src/Components/Form/Form'
+import Post from '../src/Components/Post/Post'
+
+export default (
+    <Switch>
+        <Route exact path='/' component={Auth} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/post/:postid' component={Post} />
+        <Route path='/new' component={Form} />
+    </Switch>
+)
